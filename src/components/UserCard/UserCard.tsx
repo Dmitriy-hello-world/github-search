0b837097743a3a458b81892ae9ed07e067ea6ad9
Dmitriy-hello-world <easportstng@gmail.com>
2023-05-19 13:18:17 +0300

    add info items

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -2,6 +2,7 @@ import { UserStat } from 'components/UserStat';
 import styles from './UserCard.module.scss';
 import { UserTitle } from 'components/UserTitle';
 import { LocalGithubUser } from 'types';
+import { UserInfo } from 'components/UserInfo';
 
 interface UserCardProps extends LocalGithubUser { }
 
@@ -25,6 +26,12 @@ export const UserCard = (props: UserCardProps) => {
         followers={props.followers}
         following={props.following}
         repos={props.repos}
+      />
+      <UserInfo
+        blog={props.blog}
+        company={props.company}
+        location={props.location}
+        twitter={props.twitter}
       />
     </div>
   )
